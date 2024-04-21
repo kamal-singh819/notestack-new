f46d2ef76dbdbcbcad4c760b4b39e21e3d2d7bdc
kamal singh <singh540kamal@gmail.com>
2024-04-21 22:50:19 +0530

    husky tested

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h3>This is about page</h3>
+      <p>Yes this is about page, you know about us</p>
+      <p>and you can contact us </p>
+    </div>
+  );
+};
+
+export default About;
