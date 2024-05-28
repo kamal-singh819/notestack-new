9a0ce8054c9b2210c109ef2d972f598d811d5721
kamal singh <singh540kamal@gmail.com>
2024-05-29 00:16:41 +0530

    service account variable added

--- backend/services/firebaseAdmin.js
+++ backend/services/firebaseAdmin.js
@@ -1,6 +1,20 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' };
+// import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' };
 //this json file came from settings/project-settings/service-account in firebase
+//json file convereted into below object
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
