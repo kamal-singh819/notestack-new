8d8d9a556feb15b8d389388590919cf975e9c5fb
kamal singh <singh540kamal@gmail.com>
2024-05-28 20:35:36 +0530

    some variable added related to firebase

--- frontend/src/services/FirebaseConfig.js
+++ frontend/src/services/FirebaseConfig.js
@@ -1,18 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import dotenv from 'dotenv'; dotenv.config();
 // import firebase from 'firebase/app';
 // import { getFirestore } from 'firebase/firestore';
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrWC-KZlafswmsR_YEUgWbKMz0QDW-YGc",
-    authDomain: "notetstack-new.firebaseapp.com",
-    projectId: "notetstack-new",
-    storageBucket: "notetstack-new.appspot.com",
-    messagingSenderId: "801174563864",
-    appId: "1:801174563864:web:2a4dc82e9a909c369c276d",
-    measurementId: "G-LY5831VLRS"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSEGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
