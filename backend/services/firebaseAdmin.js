4cdc1ed40c31dc1a1708fa4f93954ceb9c9a7de7
kamal singh <singh540kamal@gmail.com>
2024-05-28 23:36:33 +0530

    render url added and google signin working

--- backend/services/firebaseAdmin.js
+++ backend/services/firebaseAdmin.js
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
 import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' };
+//this json file came from settings/project-settings/service-account in firebase
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
